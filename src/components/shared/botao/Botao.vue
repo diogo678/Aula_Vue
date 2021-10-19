a37c7760351bec96e7f67a46f10ea47441effc2b
diogo678 <diohosousa186@gmail.com>
2021-10-19 10:11:23 -0300

    Curso em Vue.js

---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,59 @@
+<!-- alurapic/src/components/shared/botao/Botao.vue -->
+<template>
+  <button  @click="disparaAcao()"  v-bind:class="estiloDoBotao" :type="tipo" class="botao" >{{rotulo}}</button>
+</template>
+<script>
+export default {
+
+  props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+
+  methods: {
+
+    disparaAcao() {
+
+      if(this.confirmacao) {
+
+        if(confirm('Confirma operacao?')) {
+          this.$emit('botaoAtivado');
+        }
+        return;
+      }
+      this.$emit('botaoAtivado');
+    }
+  },
+
+  computed: {
+
+    estiloDoBotao() {
+
+      // se o valor é padrão ou não passou nada para estilo
+      if(this.estilo == 'padrao' || !this.estilo) return 'botao botao-padrao';
+
+      if(this.estilo == 'perigo') return 'botao botao-perigo';
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+.botao {
+  display: inline-block;
+  padding: 10px;
+  border-radius: 3px;
+  margin: 10px;
+  font-size: 1.2em;
+}
+
+.botao-perigo {
+  background: firebrick;
+  color: white;
+}
+
+.botao-padrao {
+  background: darkcyan;
+  color: white;
+}
+
+</style>
